a6310a78a8250462773d60cfd4680c676920125c
Mariappan Ramasamy <1221719+nappairam@users.noreply.github.com>
2022-08-09 15:32:59 +0800

    Update install script

--- bootstrap/static_files/installDocker.sh
+++ bootstrap/static_files/installDocker.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+
 sudo yum update -y
-sudo yum install docker -y
+
+sudo yum install -y jq htop
+
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
+
 sudo docker run -d -p 80:80 ealen/echo-server
